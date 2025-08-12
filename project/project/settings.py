b60e90403b72b8b315c99b4c8032cd0fb0296780
kushalyadav0 <ky1368177@gmail.com>
2025-08-12 19:58:22 +0530

    feat(database): postgres setup

--- project/project/settings.py
+++ project/project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'treedb',
+        'USER': 'treeuser',
+        'PASSWORD': 'treepassword',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
